435303228dba6512ba3f90262c0f23bae0248b35
sjpfeiffer <stevenjamespfeiffer@gmail.com>
2022-01-29 13:42:01 -0800

    Add more input validation and complete contacts/customers
    Now has basic input validation implemented for both contacts and customer scenes.
    Contact scene is completed w/ add or edit contact fully implemented
    Customer scene is completed w/ add or edit customer fully implemented

--- src/controller/AddEditContact.java
+++ src/controller/AddEditContact.java
@@ -1,15 +1,69 @@
 package controller;
 
+import database.DBConnection;
+import database.DBInteraction;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import main.Main;
+import utilities.InputValidator;
+import utilities.RemoveSquareBrackets;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AddEditContact {
+    @FXML private TextField contact_id, contact_name, contact_email;
+    @FXML private Label contact_label;
     public void initialize() {
+        final String[][] array = {null};
+        if (Main.updateDatabase) {
+            contact_label.setText("Edit Contact");
+            try {
+                Main.selectedContact.forEach((contact) -> {
+                    String selectedRow = RemoveSquareBrackets.go(contact.toString());
+                    array[0] = selectedRow.split(",");
+                });
+                String string = "SELECT * FROM contacts WHERE contact_id = '" + array[0][0] + "'";
+                PreparedStatement ps = DBConnection.getConnection().prepareStatement(string);
+                ResultSet result = ps.executeQuery();
 
+                while (result.next()) {
+                    contact_id.setText(result.getString("Contact_ID"));
+                    contact_name.setText(result.getString("Contact_Name"));
+                    contact_email.setText(result.getString("Email"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
     public void cancelContact(ActionEvent event) throws Exception {
         SceneController.changeScene("/view/Index.fxml", "Scheduler", event, false);
     }
     public void saveContact(ActionEvent event) throws Exception {
-        // Do stuff
+        if (InputValidator.textFieldFilled(contact_name, "a name") &&
+            InputValidator.textFieldFilled(contact_email, "an email") &&
+            InputValidator.isEmail(contact_email.getText())) {
+
+            String contactID = contact_id.getText();
+            String contactName = contact_name.getText();
+            String contactEmail = contact_email.getText();
+
+            if (!contactID.equals("")) {
+                String query = "UPDATE contacts SET Contact_Name = '" + contactName + "', Email = '" + contactEmail + "'" +
+                        "WHERE Contact_ID = '" + contactID + "'";
+                DBInteraction.update(query);
+            }
+            else {
+                String query = "INSERT INTO contacts (Contact_Name, Email) " +
+                        "VALUES ('" + contactName + "', '" + contactEmail + "')";
+                System.out.println(query);
+                DBInteraction.update(query);
+            }
+            SceneController.changeScene("/view/Index.fxml", "Scheduler", event, false);
+        }
     }
 }
--- src/utilities/InputValidator.java
+++ src/utilities/InputValidator.java
@@ -1,14 +1,100 @@
 package utilities;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class InputValidator {
 
+    /**
+     * Checks if the user actually filled in a textfield with text
+     * @param textField The textfield to check
+     * @param string The string provided by the user
+     * @return true if the textfield is not empty
+     */
     public static boolean textFieldFilled(TextField textField, String string) {
         if (textField.getText().isEmpty()) {
             Popup.errorAlert("Error", "Make sure you input " + string + "!");
             return false;
         }
         return true;
+    }
+
+    /**
+     * Checks if the user actually selected an option in a combobox
+     * @param comboBox The combobox to check
+     * @param string The string provided by the user
+     * @return true if an option is selected
+     */
+    public static boolean comboBoxSelected(ComboBox comboBox, String string) {
+        if (comboBox.getValue() == null) {
+            Popup.errorAlert("Error", "Make sure you select " + string + "!");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Checks provided email string for correctness
+     * @param string The string to be checked
+     * @return true if string is valid
+     * @throws FileNotFoundException
+     */
+    public static boolean isEmail(String string) throws FileNotFoundException {
+        if (string.contains("@") && string.contains(".")) {
+
+            /** Split the email address up into parts */
+            String domain = string.substring(string.indexOf("@") + 1);
+            domain = domain.substring(0, domain.indexOf("."));
+            String topLevelDomain = string.substring(string.indexOf(".") + 1);
+            String userName = string.substring(0, string.indexOf("@"));
+
+            /**
+             *  Check if any of the substrings are empty, and validate TLD
+             *  Note: A TLD is the text at the end of a URL. (.com, .org, etc.)
+             */
+            if (domain.isEmpty() || topLevelDomain.isEmpty() || userName.isEmpty() || !isValidTLD(topLevelDomain)) {
+                Popup.errorAlert("Error", "Make sure you input the email correctly!");
+                return false;
+            }
+            return true;
+        }
+        Popup.errorAlert("Error", "Make sure you input the email correctly!");
+        return false;
+    }
+
+    /**
+     * Checks email Top Level Domain (TLD) against text file with list of possible TLDs
+     * @param string The string to be compared
+     * @return true if match was found
+     * @throws FileNotFoundException
+     */
+    public static boolean isValidTLD(String string) throws FileNotFoundException {
+        File file = new File("src/resources/tlds-alpha-by-domain.txt");
+        Scanner input = new Scanner(file);
+        while (input.hasNextLine()) {
+            if (input.nextLine().equalsIgnoreCase(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks to make sure appointment start and end times are between normal business hours
+     * @param string The time to compare to business hours in 24hr format
+     * @return Whether or not the provided time is between normal business hours
+     */
+    public static boolean isBusinessHours(String string) {
+        //* Business hours are between 8 AM and 10 PM EST, including weekends. */
+        return false;
+    }
+
+    public static boolean isCustomerOverlap() {
+        return false;
     }
 }
